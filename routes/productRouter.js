17b881fd9332675309e814e16861ad2571eab40c
Pabindu <pabinduamodya12@gmail.com>
2025-02-18 21:44:59 +0530

    get product by id function✌️

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPtoduct, deleteProduct, getProduct, updateProduct } from "../controllers/productController.js";
+import { createPtoduct, deleteProduct, getProduct, getProductById, updateProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",createPtoduct)
 productRouter.get("/",getProduct)
+productRouter.get("/:productId",getProductById)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
 
