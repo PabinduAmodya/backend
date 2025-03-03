5bb9611f92c11a849964cbc50015ef2fd05e622e
Pabindu <pabinduamodya12@gmail.com>
2025-03-03 11:01:04 +0530

    search bar function

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { createPtoduct, deleteProduct, getProduct, getProductById, updateProduct } from "../controllers/productController.js";
+import { createPtoduct, deleteProduct, getProduct, getProductById, searchproducts, updateProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/",createPtoduct)
 productRouter.get("/",getProduct)
+productRouter.get("/search/:query", searchproducts)
 productRouter.get("/:productId",getProductById)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
+  
 
 export default productRouter;
